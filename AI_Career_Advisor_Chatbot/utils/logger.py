c1d99df6464647e5107af0a1d96ffb263f785d67
muzahidshaik04 <muzahidshaik04@gmail.com>
2026-02-21 22:19:59 +0530

    Add logger utility for logging application events

---
+++ AI_Career_Advisor_Chatbot/utils/logger.py
@@ -0,0 +1,16 @@
+import logging
+import os
+
+LOG_DIR = "logs"
+
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+logging.basicConfig(
+    filename=os.path.join(LOG_DIR, "app.log"),
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def get_logger():
+    return logging.getLogger()
